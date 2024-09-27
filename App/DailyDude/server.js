34002f3de1112a951fcee9880f93218ef7db87d9
BreezyBce <66754251+BreezyBce@users.noreply.github.com>
2024-09-26 18:18:56 -0700

    Update server.js

--- App/DailyDude/server.js
+++ App/DailyDude/server.js
@@ -15,8 +15,8 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
-app.use(cors());
-app.use(express.json());
+app.use(express.static(path.join(__dirname, 'dist')));
+
 
 const GOOGLE_API_KEY = process.env.GOOGLE_TRANSLATE_API_KEY;
 const EXCHANGE_RATE_API_KEY = process.env.EXCHANGE_RATE_API_KEY;
