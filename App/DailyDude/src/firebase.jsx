a7296271ec3b834e19878c8fdbf730c44a7ae9ca
BreezyBce <66754251+BreezyBce@users.noreply.github.com>
2024-09-26 18:06:50 -0700

    Update firebase.jsx

--- App/DailyDude/src/firebase.jsx
+++ App/DailyDude/src/firebase.jsx
@@ -6,12 +6,13 @@ import { getStorage } from 'firebase/storage'; // Add this import
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBi0Nucmk6Kw530UBVCd0uA9edAg-uuLHA",
-  authDomain: "dailydude-30ca7.firebaseapp.com",
-  projectId: "dailydude-30ca7",
-  storageBucket: "dailydude-30ca7.appspot.com",
-  messagingSenderId: "863507486470",
-  appId: "1:863507486470:web:091b7d9263d33e1dda3087"
+  apiKey: "AIzaSyCh5oZ-WBNqNHsQtfDC5m8ZP5XTImS7MMY",
+  authDomain: "dailydude-40c21.firebaseapp.com",
+  projectId: "dailydude-40c21",
+  storageBucket: "dailydude-40c21.appspot.com",
+  messagingSenderId: "691813705913",
+  appId: "1:691813705913:web:65c54df1caa36837d01368",
+  measurementId: "G-31RPJ5M9QC"
 };
 
 // Initialize Firebase
