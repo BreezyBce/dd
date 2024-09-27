52445d11999cbe327f828fea2de241b2e04e06dd
BreezyBce <66754251+BreezyBce@users.noreply.github.com>
2024-09-26 18:13:06 -0700

    Update vite.config.js

--- App/DailyDude/vite.config.js
+++ App/DailyDude/vite.config.js
@@ -1,34 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src')
-    }
-  },
   build: {
-    target: 'es2015',
-    minify: 'terser',
-    terserOptions: {
-      compress: {
-        drop_console: true,
-        drop_debugger: true
-      },
-      mangle: {
-        safari10: true,
-      },
-      format: {
-        safari10: true,
-      }
-    }
+    outDir: 'dist',
+    chunkSizeWarningLimit: 10000,
   },
-  server: {
-    host: '0.0.0.0',
-    hmr: {
-      clientPort: 443
-    }
-  }
-})
+});
