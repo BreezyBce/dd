47f4e86d90c45796a1ff27d4c05f65c4df121066
BreezyBce <66754251+BreezyBce@users.noreply.github.com>
2024-10-01 01:46:35 -0700

    Update SubscriptionContext.jsx

--- App/DailyDude/src/SubscriptionContext.jsx
+++ App/DailyDude/src/SubscriptionContext.jsx
@@ -46,4 +46,4 @@ export const SubscriptionProvider = ({ children }) => {
   );
 };
 
-export const useSubscription = () => useContext(SubscriptionContext);
+export const useSubscription = () => useContext(SubscriptionContext); 
