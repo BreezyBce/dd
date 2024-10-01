7514f2c6a8c6e4e0d8f3df4a85acafba7f168773
BreezyBce <66754251+BreezyBce@users.noreply.github.com>
2024-10-01 13:52:41 -0700

    Update create-checkout-session.js

--- App/DailyDude/api/create-checkout-session.js
+++ App/DailyDude/api/create-checkout-session.js
@@ -1,29 +1,28 @@
-import Stripe from 'stripe';
-
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 export default async function handler(req, res) {
   if (req.method === 'POST') {
     try {
       const { priceId, userId } = req.body;
 
+      // Create Checkout Sessions from body params.
       const session = await stripe.checkout.sessions.create({
-        mode: 'subscription',
-        payment_method_types: ['card'],
         line_items: [
           {
-            price: priceId,
+            // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
+            price: price_1PxwpuA9AcwovfpkLQxWKcJo,
             quantity: 1,
           },
         ],
-        success_url: `${process.env.DOMAIN}/success?session_id={CHECKOUT_SESSION_ID}`,
-        cancel_url: `${process.env.DOMAIN}/canceled`,
+        mode: 'subscription',
+        success_url: `${req.headers.origin}/?success=true&session_id={CHECKOUT_SESSION_ID}`,
+        cancel_url: `${req.headers.origin}/?canceled=true`,
         client_reference_id: userId,
       });
 
       res.status(200).json({ url: session.url });
-    } catch (error) {
-      res.status(500).json({ error: error.message });
+    } catch (err) {
+      res.status(err.statusCode || 500).json(err.message);
     }
   } else {
     res.setHeader('Allow', 'POST');
