54f3314b1b9925dc72b587a7592767ca93b1a149
BreezyBce <66754251+BreezyBce@users.noreply.github.com>
2024-09-30 23:24:02 -0700

    Update translate.js

--- App/DailyDude/api/translate.js
+++ App/DailyDude/api/translate.js
@@ -1,6 +1,10 @@
 import axios from 'axios';
 
 export default async function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  res.setHeader('Access-Control-Allow-Origin', '*');
+
+export default async function handler(req, res) {
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method not allowed' });
   }
