f896732472f0c1ab5d493c2f112dcfce49109688
BreezyBce <66754251+BreezyBce@users.noreply.github.com>
2024-10-03 00:34:57 -0700

    Update Clock.jsx

--- App/DailyDude/src/components/Clock.jsx
+++ App/DailyDude/src/components/Clock.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { FaPlay, FaPause, FaStop, FaPlus, FaTrash } from 'react-icons/fa';
 
 const Clock = () => {
