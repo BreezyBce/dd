c339174f861d0516bf76d27199a46b236a110804
BreezyBce <66754251+BreezyBce@users.noreply.github.com>
2024-10-08 15:16:57 -0700

    Update SignupPage.jsx

--- App/DailyDude/src/components/SignupPage.jsx
+++ App/DailyDude/src/components/SignupPage.jsx
@@ -139,7 +139,8 @@ const SignupPage = () => {
                     <p>Or</p>
           <div class="flex justify-center mt-4 space-x-4">
         <button onClick={handleGoogleSignup} className="w-full bg-white text-gray-700 py-2 px-4 rounded-md border border-gray-300 hover:bg-gray-100 flex items-center justify-center" disabled={!isOnline}
-          ></div> <FaGoogle className="mr-2" /> Sign up with Google</button>
+          ><FaGoogle className="mr-2" /> Sign up with Google</button>
+            </div> 
         </div>
         <div className="mt-4 text-center">
           <p className="text-sm text-gray-600">
