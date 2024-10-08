dffdd36fc34ecd87fb2d913a565d46ffee64c486
BreezyBce <66754251+BreezyBce@users.noreply.github.com>
2024-10-08 00:45:17 -0700

    Update CurrencyConverter.jsx

--- App/DailyDude/src/components/CurrencyConverter.jsx
+++ App/DailyDude/src/components/CurrencyConverter.jsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import withSubscription from '../withSubscription';
+import { useSubscription } from '../SubscriptionContext';
+import UpgradeButton from './UpgradeButton';
 
 const CurrencyConverter = () => {
   const [amount, setAmount] = useState(1);
